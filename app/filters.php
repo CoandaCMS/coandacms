908ef7d576ee2e447b0e81996134422c7b8e5f33
Alex Dover <me@alexdover.co.uk>
2014-03-17 20:54:23 +0000

    Initial commit.

---
+++ app/filters.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application & Route Filters
+|--------------------------------------------------------------------------
+|
+| Below you will find the "before" and "after" events for the application
+| which may be used to do any work before or after a request into your
+| application. Here you may also register your custom route filters.
+|
+*/
+
+App::before(function($request)
+{
+	//
+});
+
+
+App::after(function($request, $response)
+{
+	//
+});
+
+/*
+|--------------------------------------------------------------------------
+| CSRF Protection Filter
+|--------------------------------------------------------------------------
+|
+| The CSRF filter is responsible for protecting your application against
+| cross-site request forgery attacks. If this special token in a user
+| session does not match the one given in this request, we'll bail.
+|
+*/
+
+Route::filter('csrf', function()
+{
+	if (Session::token() != Input::get('_token'))
+	{
+		throw new Illuminate\Session\TokenMismatchException;
+	}
+});
---
+++ readme.md
@@ -0,0 +1,11 @@
+## Coanda CMS
+
+**Work in progress** I would strongly advise not using this!
+
+Coanda is (going to be) a CMS based on the superb [Laravel framework](http://laravel.com)
+
+I've worked with many different CMS platforms over the last decade or so and feel it is time to try and create my own.
+
+Over the next few weeks I hope to get the basics in place.
+
+Tweet me if you like [@alexdover](http://alexdover.co.uk).
