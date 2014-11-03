0bc41cf7c4da1349b4017d7e596dbe6a07786751
Alex Dover <me@alexdover.co.uk>
2014-11-03 13:39:05 +0000

    Removed duplicate <title> tags
    There was a duplicate title tag in the layout template.

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -8,7 +8,6 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Coanda CMS</title>
 
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/bootstrap/3.3.0/css/bootstrap.min.css">
